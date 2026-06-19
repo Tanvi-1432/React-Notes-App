95e4bb1454285789339c81cbba4e59eccba0b620
Tanvi <105012337+Tanvi-1432@users.noreply.github.com>
2026-06-19 12:30:10 -0400

    Fix Vercel asset paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/React-Notes-App/',
+  base: './',
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
